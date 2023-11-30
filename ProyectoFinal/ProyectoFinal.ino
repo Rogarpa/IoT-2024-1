60b83c05995fcb6ae5bbcd7f022f53497ebf20de
Rodrigo <rodrigogarciapadilla@ciencias.unam.mx>
2023-11-30 11:18:21 -0600

    Added basic structure for project     add ProyectoFinal/ProyectoFinal.ino

---
+++ ProyectoFinal/ProyectoFinal.ino
@@ -0,0 +1,33 @@
+/*
+  Universidad Nacional Autónoma de México
+  Facultad de Ciencias
+  Licenciatura en Ciencias de la Computación
+  Seminario de Ciencias de la Computación A: Introducción al Internet de las Cosas
+
+  Escrito por:
+  García Padrilla Rodrigo 
+  Giron Jimenez Emmanuel
+  Última modificación: 30-diciembre-2023
+*/
+void setup() {
+    setupBot();
+    setupAlarma();
+}
+
+void setupBot(){
+}
+
+void setupAlarma(){
+}
+
+
+void loop() {
+    loopBot();
+    loopAlarma();
+}
+
+void loopBot(){
+}
+
+void loopAlarma(){
+}
